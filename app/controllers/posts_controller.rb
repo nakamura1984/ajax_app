8da8ae00d48418ecd34a7dadb01c0ebcd94f5af3
nakamura1984 <126934364+nakamura1984@users.noreply.github.com>
2023-03-15 16:23:56 +0900

    リクエストを処理する

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,7 +5,8 @@ class PostsController < ApplicationController
   end
 
   def create
+    post = Post.create(content: params[:content])
     Post.create(content: params[:content])
-    redirect_to action: :index
+    render json:{ post: post }
   end
 end
